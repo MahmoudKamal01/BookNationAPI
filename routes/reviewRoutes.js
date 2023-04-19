bc94c806a7b2ffa082e70e02a2e07ad17cfa4d80
Halaalaa68 <66278563+Halaalaa68@users.noreply.github.com>
2023-04-20 00:11:05 +0200

    Update reviewRoutes.js

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -6,6 +6,8 @@ const reviewController = require("../controllers/reviewController.js");
 reviewRouter.route("/:bookId/reviews").get(reviewController.getAllReviews);
 // Get a review by id
 reviewRouter.route("/:bookId/reviews/:id").get(reviewController.getReview);
+// Edit a review by id
+reviewRouter.route("/:bookId/reviews/:id").put(reviewController.editReview);
 module.exports = {
   reviewRouter,
 };
