f88e608c7466680cfc9010cb2011be630b9846a0
Halaalaa68 <66278563+Halaalaa68@users.noreply.github.com>
2023-04-20 00:05:06 +0200

    Update reviewController.js

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -20,6 +20,15 @@ const getReview = async (req, res) => {
   }
 };
 
+const editReview = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const review = await Review.findByIdAndUpdate(id, req.body);
+    res.status(200).send(req.body);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
 
 module.exports = {
   getAllReviews,
