afe55074174986c1ec27ee628e4606041476fde6
mohamedkhaledmk <66388583+mohamedkhaledmk@users.noreply.github.com>
2023-04-19 23:40:33 +0200

    updating book schema

--- models/book.js
+++ models/book.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
+
 const bookSchema = mongoose.Schema({
-title: {type:String},
-author: {type:String},
-description: {type:String},
-release: {type:Date},
-price: {type:Number},
-category:{type:String},
-})
+  title: { type: String },
+  author: { type: String },
+  description: { type: String },
+  release: { type: Date },
+  price: { type: Number },
+  category: { type: String },
+  reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }],
+});
 
 const Book = mongoose.models.Book || mongoose.model('Book', bookSchema);
 
 module.exports = {
-    Book,
+  Book,
 };
-
