ba82424e37f8d601e3bdee04b049ba25e8ab1cea
mohamedkhaledmk <66388583+mohamedkhaledmk@users.noreply.github.com>
2023-04-19 23:41:07 +0200

    updating reviews schema

--- models/review.js
+++ models/review.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
-
-
+const reviewSchema = mongoose.Schema({
+  userName: { type: String },
+  comment: { type: String },
+  rating: { type: Number },
+});
 
 const Review = mongoose.model('Review', reviewSchema);
 
