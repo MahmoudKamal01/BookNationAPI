fa88577bf0420cd905d58314935c251c871ec129
Nouran-Alaa <66510857+Nouran-Alaa@users.noreply.github.com>
2023-04-20 00:13:53 +0200

    Add Delete Route

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -8,6 +8,11 @@ reviewRouter.route("/:bookId/reviews").get(reviewController.getAllReviews);
 // Create a review for a book
 reviewRouter.route("/:bookId/reviews").post(reviewController.createReview);
 
+// Delete a review by id
+reviewRouter
+  .route("/:bookId/reviews/:id")
+  .delete(reviewController.deleteReview);
+
 module.exports = {
   reviewRouter,
 };
